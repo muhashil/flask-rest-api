6414e5dc662816537e2b2cf85d5429ab07596d00
Muhammad Ashil <muh.ashil@yahoo.com>
2020-09-29 10:26:01 +0700

    update

---
+++ resources/store.py
@@ -0,0 +1,33 @@
+from flask_restful import Resource, reqparse
+from models.store import StoreModel
+
+class Store(Resource):
+    # parser = reqparse.RequestParser()
+    # parser.add_argument('name', type=str, required=True, help='This field cannot be blanked.')
+
+    def get(self, name):
+        store = StoreModel.find_by_name(name=name)
+        if store:
+            return store.json(), 200
+        return {'message': f'Store with name `{name}` not found.'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': f'Store with name `{name}` already exists.'}
+
+        store = StoreModel(name=name)
+        try:
+            store.save_to_db()
+        except Exception:
+            return {'message': 'Error when inserting store'}, 500
+        return {'message': 'Store successfully created.'}, 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name=name)
+        if store:
+            store.delete_from_db()
+        return {'message': 'Store deleted.'}, 204
+
+class StoreList(Resource):
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.find_all()]}
